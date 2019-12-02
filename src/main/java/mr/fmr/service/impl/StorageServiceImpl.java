00f81de65e6b593ac89f00dfb80c821c84c8dca6
marcosrodriigues <marcos.rodriiigues@gmail.com>
2019-12-02 01:34:30 -0300

    Deploy no google cloud funcionando para o backend

--- src/main/java/mr/fmr/service/impl/StorageServiceImpl.java
+++ src/main/java/mr/fmr/service/impl/StorageServiceImpl.java
@@ -1,93 +1,128 @@
 package mr.fmr.service.impl;
 
-import mr.fmr.exception.FileStorageException;
-import mr.fmr.exception.MyFileNotFoundException;
-import mr.fmr.pojo.FileStorageProperties;
+import com.google.cloud.storage.Acl;
+import com.google.cloud.storage.BlobInfo;
+import com.google.cloud.storage.Storage;
+import com.google.cloud.storage.StorageOptions;
 import mr.fmr.service.StorageService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
-import org.springframework.core.io.UrlResource;
 import org.springframework.stereotype.Service;
-import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
-import java.io.IOException;
-import java.net.MalformedURLException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+//import java.nio.file.Files;
+//import java.nio.file.Path;
+//import java.nio.file.Paths;
+
 
 @Service
 public class StorageServiceImpl implements StorageService {
 
-    private Path fileStorageLocation;
+    //private Path fileStorageLocation;
 
-    @Autowired
-    public StorageServiceImpl(FileStorageProperties properties) {
-        this.fileStorageLocation = Paths.get(properties.getUploadDir()).toAbsolutePath().normalize();
+    private static Storage storage = null;
+    private final String BUCKET_NAME = "frm_api";
 
-        try {
-            Files.createDirectories(this.fileStorageLocation);
-        } catch (Exception e) {
-            throw new FileStorageException("Não foi possível criar o caminho");
-        }
+    static {
+        storage = StorageOptions.getDefaultInstance().getService();
     }
+
+//    @Autowired
+//    public StorageServiceImpl(FileStorageProperties properties) {
+//
+//        this.fileStorageLocation = Paths.get(properties.getUploadDir()).toAbsolutePath().normalize();
+//
+//        try {
+//            Files.createDirectories(this.fileStorageLocation);
+//        } catch (Exception e) {
+//            throw new FileStorageException("Não foi possível criar o caminho");
+//        }
+//    }
 
 
     @Override
     public String store(MultipartFile file) {
-        String filename = StringUtils.cleanPath(file.getOriginalFilename());
+        Date d = new Date();
+        final String fileName = file.getOriginalFilename() + d.getTime();
 
         try {
-            if(filename.contains("..")) {
-                throw new FileStorageException("Filename contém caminho inválido");
+            InputStream in = file.getInputStream();
+            ByteArrayOutputStream os = new ByteArrayOutputStream();
+
+            byte[] readBuf = new byte[4096];
+            while(in.available() > 0) {
+                int bytesRead = in.read(readBuf);
+                os.write(readBuf, 0, bytesRead);
             }
+            BlobInfo info =
+                    storage.create(
+                            BlobInfo
+                            .newBuilder(BUCKET_NAME , fileName)
+                            .setAcl(new ArrayList<>(Arrays.asList(Acl.of(Acl.User.ofAllUsers(), Acl.Role.READER))))
+                            .build());
 
-            Path targetLocation = this.fileStorageLocation.resolve(filename);
+            return info.getMediaLink();
 
-            int count = 1;
-            while(targetLocation.toFile().exists()) {
-                String fullPathWithoutFilename = "/";
-                for (String tinyPath : targetLocation.toFile().getAbsolutePath().split("/")) {
-                    if (!tinyPath.isEmpty())
-                        fullPathWithoutFilename += tinyPath + "/";
-
-                    if (tinyPath.equalsIgnoreCase("upload")) break;
-                }
-
-                String fullname = targetLocation.toFile().getName();
-                String name = fullname.split("\\.")[0];
-                String ext = fullname.split("\\.")[1];
-                filename = name + "_" + count + "." + ext;
-                File novoFile = new File(fullPathWithoutFilename + filename);
-
-                targetLocation.toFile().renameTo(novoFile);
-            }
-
-            Files.copy(file.getInputStream(), targetLocation);
-
-            return filename;
-        } catch (IOException e) {
-            throw new FileStorageException("Não foi possível armazenar o arquivo" + filename + "!Tente novamente", e);
+        } catch (Exception e) {
+            e.printStackTrace();
         }
+
+        return null;
+
+//        try {
+//            if(filename.contains("..")) {
+//                throw new FileStorageException("Filename contém caminho inválido");
+//            }
+//
+//            Path targetLocation = this.fileStorageLocation.resolve(filename);
+//
+//            int count = 1;
+//            while(targetLocation.toFile().exists()) {
+//                String fullPathWithoutFilename = "/";
+//                for (String tinyPath : targetLocation.toFile().getAbsolutePath().split("/")) {
+//                    if (!tinyPath.isEmpty())
+//                        fullPathWithoutFilename += tinyPath + "/";
+//
+//                    if (tinyPath.equalsIgnoreCase("upload")) break;
+//                }
+//
+//                String fullname = targetLocation.toFile().getName();
+//                String name = fullname.split("\\.")[0];
+//                String ext = fullname.split("\\.")[1];
+//                filename = name + "_" + count + "." + ext;
+//                File novoFile = new File(fullPathWithoutFilename + filename);
+//
+//                targetLocation.toFile().renameTo(novoFile);
+//            }
+//
+//            Files.copy(file.getInputStream(), targetLocation);
+//
+//            return filename;
+//        } catch (IOException e) {
+//            throw new FileStorageException("Não foi possível armazenar o arquivo" + filename + "!Tente novamente", e);
+//        }
     }
 
 
     @Override
     public Resource loadAsResource(String filename) {
-        try {
-            Path file = this.fileStorageLocation.resolve(filename).normalize();
-
-            Resource resource = new UrlResource(file.toUri());
-
-            if (resource.exists()) {
-                return resource;
-            }
-
-            throw new MyFileNotFoundException("Arquivo " + filename + " não encontrado!");
-        } catch (MalformedURLException e) {
-            throw new MyFileNotFoundException("Arquivo " + filename + " não encontrado!", e);
-        }
+//        try {
+//            Path file = this.fileStorageLocation.resolve(filename).normalize();
+//
+//            Resource resource = new UrlResource(file.toUri());
+//
+//            if (resource.exists()) {
+//                return resource;
+//            }
+//
+//            throw new MyFileNotFoundException("Arquivo " + filename + " não encontrado!");
+//        } catch (MalformedURLException e) {
+//            throw new MyFileNotFoundException("Arquivo " + filename + " não encontrado!", e);
+//        }
+        return null;
     }
 }
